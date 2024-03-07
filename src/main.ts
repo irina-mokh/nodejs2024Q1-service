ce128954b71df9b4635ec42f67708e5da78dd315
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2024-03-07 18:33:45 +0300

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFileSync } from 'node:fs';
+import { join } from 'node:path';
+import { parse } from 'yaml';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const apiFile = readFileSync(join(__dirname, '../doc/api.yaml'), 'utf8');
+
+  SwaggerModule.setup('doc', app, parse(apiFile));
+
   await app.listen(4000);
 }
 bootstrap();
